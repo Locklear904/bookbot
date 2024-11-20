f461a315861c8fa274b3f6a7197223b2808ebc76
Locklear904 <locklear904@gmail.com>
2024-11-20 10:27:54 -0500

    Add functions to organize list and print report

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(count_words(file_contents))
-        print(count_chars(file_contents))
+        print_report(f.name, file_contents)
 
 def count_words(string):
     split_string = string.split()
@@ -20,5 +19,28 @@ def count_chars(string):
         else:
             char_dict[lower_char] = 1
     return char_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def dict_to_list(dict):
+    list = []
+    for key in dict:
+        entry = {
+            "char": key,
+            "count": dict[key]
+        }
+        list.append(entry)
+    return list
+
+def print_report(file_name, file_contents):
+    word_count = count_words(file_contents)
+    char_count = count_chars(file_contents)
+    char_count_list = dict_to_list(char_count)
+    char_count_list.sort(reverse=True, key=sort_on)
+    print(f'--- Begin report of {file_name} ---\n {word_count} words found in the document\n\n')
+    for entry in char_count_list:
+        print(f"The '{entry["char"]}' character was found {entry["count"]} times")
+    print("--- End report ---")
 
 main()
